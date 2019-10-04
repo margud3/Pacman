dcdc60317cfee64f7f675b2c338cb6604d6d4241
Giedrius <giedrius.valunas@ktu.edu>
2019-10-04 12:11:22 +0300

    DB init

---
+++ Pacman/Pacman/Migrations/20191004090609_init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pacman.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LastUpdated = table.Column<DateTime>(nullable: false),
+                    name = table.Column<string>(nullable: true),
+                    score = table.Column<int>(nullable: false),
+                    posX = table.Column<double>(nullable: false),
+                    posY = table.Column<double>(nullable: false),
+                    boosted = table.Column<bool>(nullable: false),
+                    ghost = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
